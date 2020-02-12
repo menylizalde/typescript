198d84ef9d51ed2faefb50eed82c307963dc3bc2
menylizalde <ovnitech2016@gmail.com>
2020-02-12 00:39:05 -0600

    Prectica 2. La brocha Gorda. Cotizador TERMINADO

--- practica2/js/app.js
+++ practica2/js/app.js
@@ -1,8 +1,8 @@
 
 /**Se declaran Variables */
-let numero1;
-let numero2;
-let numero3;
+let alturamuro;
+let longitudmuro;
+let precioporM;
 let costo;
 
 /**Se declara la variable button y se le asigna el boton calcular */
@@ -11,11 +11,11 @@ let button = document.getElementById('btnCotizar');
 
 button?.addEventListener('click', multiplica)
 function multiplica(){
-    numero1= Number( document.getElementById('altura').value);
-    numero2= Number( document.getElementById('longitud').value);
-    numero3= Number( document.getElementById('costopormetro2').value);
+    alturamuro= Number( document.getElementById('altura').value);
+    longitudmuro= Number( document.getElementById('longitud').value);
+    precioporM= Number( document.getElementById('costopormetro2').value);
     
-    costo= (numero1 * numero2 * numero3);
+    costo= (alturamuro * longitudmuro * precioporM);
     let CostoTotal=document.getElementById('CostoTotal')
     CostoTotal.innerText = costo;
 }
