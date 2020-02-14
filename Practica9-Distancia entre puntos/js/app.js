3fa3ac3ac4f6fb5545cba813fd172ea772b31748
menylizalde <ovnitech2016@gmail.com>
2020-02-13 18:51:24 -0600

    Practica 9-Calculo de distancia entre dos puntos

---
+++ Practica9-Distancia entre puntos/js/app.js
@@ -0,0 +1,24 @@
+
+
+
+let X1;
+let X2;
+let Y1;
+let Y2;
+let distancia;
+
+
+let button=document.getElementById('btnDistancia');
+button.addEventListener('click',distancia12)
+
+function distancia12(){
+    X1=Number(document.getElementById('inpValorX1').value);
+    X2=Number(document.getElementById('inpValorX2').value);
+    Y1=Number(document.getElementById('inpValorY1').value);
+    Y2=Number(document.getElementById('inpValorY2').value);
+
+    distancia= Math.sqrt( Math.pow((X2-X1),2) + Math.pow((Y2-Y1),2) );
+    let ImprimirDistancia=document.getElementById('ImprimirDistancia');
+    ImprimirDistancia.innerText=distancia
+
+}
