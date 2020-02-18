47db211c6c3c36ededc7cf3eca35db8e561b7e39
menylizalde <ovnitech2016@gmail.com>
2020-02-18 08:54:19 -0600

    Condicionales4-Descuento en Compra

---
+++ Condicionales4- Descuento/app.js
@@ -0,0 +1,23 @@
+const button=document.getElementById('btnCalcular');
+button.addEventListener('click',CalculoDescuento);
+let ValorCompra;
+let Descuento;
+
+function CalculoDescuento(){
+    let ValorCompra=Number(document.getElementById('inpValorCompra').value);
+    let Descuento=Number(document.getElementById('inpDescuento').value);
+        const PrintAlert=document.getElementById('PrintAlert');
+
+   
+
+    if (ValorCompra <=1000){
+        console.log('No Aplica Descuento');
+        PrintAlert.innerText=('No Aplica Descuento')
+    }
+     else{
+         console.log('Su descuento es de:' + ValorCompra*.2);
+         PrintAlert.innerText=('Su Descuento es de: ' + ValorCompra*.2);
+     }
+
+
+}
