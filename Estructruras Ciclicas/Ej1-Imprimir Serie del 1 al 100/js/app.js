a890aa23eaf4d8b94da3d6af3a3b0cae07f67bd0
menylizalde <ovnitech2016@gmail.com>
2020-02-27 10:15:37 -0600

    Imprimir Serie

---
+++ Estructruras Ciclicas/Ej1-Imprimir Serie del 1 al 100/js/app.js
@@ -0,0 +1,11 @@
+function eventListener(){
+    const button= document.getElementById('btnInit');
+    button.addEventListener('click', SerieNumerica);
+}
+
+addEventListener()
+
+function InitApplication(){
+    let acumulador=0;
+    const
+}
