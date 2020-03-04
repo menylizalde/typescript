5db2c39123491a6521bf7ef5c8874a30dda58090
menylizalde <ovnitech2016@gmail.com>
2020-03-03 19:39:01 -0600

    1

--- Estructruras Ciclicas/Ej9- Numero Fibonacci/app.js
+++ Estructruras Ciclicas/Ej9- Numero Fibonacci/app.js
@@ -24,15 +24,13 @@ function InitApplitacion(){
            li.innerText=`EL NUMERO ${itercaciones} SI PERTENECE A LA SERIE. ASI COMO ${itercaciones-NumeroX} Y ${itercaciones+NumeroX}`;
            // imprimir resultado en:
            ListGroup.appendChild(li)
+
+
         }
-        else{
+        else if(itercaciones!==Resultado){
             console.log(`${itercaciones} no pertenece a la Serie`)
-            const li=document.createElement('li');
+            
         }
-        li.setAttribute('class', 'list-group-item');
-        li.innerText=`EL NUMERO ${itercaciones} NO PERTENECE A LA SERIE. ASI COMO ${itercaciones-NumeroX} Y ${itercaciones+NumeroX}`;
-        // imprimir resultado en:
-        ListGroup.appendChild(li)
         // const li = document.createElement('li'); /*creamos el elemento li, con createElement*/
         // li.setAttribute('class', 'list-group-item'); // para dar estilo al li
         // li.innerText=Resultado;
