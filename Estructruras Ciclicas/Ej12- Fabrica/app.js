6650231d5ff9ee4c49390d3960902f1df1976e09
menylizalde <ovnitech2016@gmail.com>
2020-03-06 09:32:47 -0600

    Fabrica

--- Estructruras Ciclicas/Ej12- Fabrica/app.js
+++ Estructruras Ciclicas/Ej12- Fabrica/app.js
@@ -13,21 +13,22 @@ function IniciarAplicacion(){
     for(let i = 1; i<=Empresa; i++){
         let clave=Number(prompt(`Ingresa la Clave Empresa ${i}:`));
         const ListGroup = document.getElementById('Resultados');
-        // console.log(`Empresa${i} clave ${clave}.`)
-        // acumulador+=clave;
+        acumulador=0;
+
         for(let j=1; j<=12; j++){
             let produccion=Number(prompt(`Ingresa la Produccion del Mes ${j}`));
             acumulador=acumulador+produccion
             console.log(`Empresa${i} clave ${clave}. Produccion ${acumulador}`)
         }
+
         if (acumulador>3000000) {
             
-            const li = document.createElement('li'); /*creamos el elemento li, con createElement*/
-            li.setAttribute('class', 'list-group-item'); // para dar estilo al li
-            li.innerText=`Empresa: ${i}. Clave: ${clave}  Produccion: ${acumulador}`;
-            // para difinir la posicion del li con la propiedad appendChild
-            ListGroup.appendChild(li);
         }
+        const li = document.createElement('li'); /*creamos el elemento li, con createElement*/
+        li.setAttribute('class', 'list-group-item'); // para dar estilo al li
+        li.innerText=`Empresa: ${i}. Clave: ${clave}  Produccion: ${acumulador}`;
+        // para difinir la posicion del li con la propiedad appendChild
+        ListGroup.appendChild(li);
 
     }
 }
