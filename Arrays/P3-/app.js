0e1dcff5a53b86016524850e1edfd5aca5b463a2
menylizalde <ovnitech2016@gmail.com>
2020-03-13 10:45:24 -0600

    Practica 3

--- Arrays/P3-/app.js
+++ Arrays/P3-/app.js
@@ -1,3 +1,5 @@
+let Print=document.getElementById('PrintZone')
+const output= document.getElementById("output")
 function eventListener(){
     document.getElementById('MostrarInputs').addEventListener('click', mostrarInputs);
 }
@@ -5,12 +7,16 @@ eventListener();
 // Creamos Inputs
 function mostrarInputs(){
     const NumeroInputs=Number(document.getElementById('NumeroInputs').value);
-
-    if(NumeroInputs>0){
+    
+    if (NumeroInputs>1000 || NumeroInputs<0){
+        return
+    }
+    EliminarElementos();
+    
         const row= document.createElement('div');
         row.setAttribute("clas", "row")
+
         for(let i=0; i<NumeroInputs; i++){
-            const output= document.getElementById("output")
             const divCol=document.createElement("div");
             divCol.setAttribute("class", "col-12");
 
@@ -32,6 +38,48 @@ function mostrarInputs(){
         }
         output.appendChild(row);
 
+        const buttonObtenerResultado= document.createElement("button");
+        buttonObtenerResultado.setAttribute('class', 'btn btn-outline-success btn-block');
+        buttonObtenerResultado.setAttribute('id', 'BtnCalcularResultado');
+        buttonObtenerResultado.innerText='Resultado';
+        row.appendChild(buttonObtenerResultado);
+
+        document.getElementById('BtnCalcularResultado').addEventListener('click', RealizarComparacion)
+}
+let arreglo1=[];
+
+function RealizarComparacion(){
+    const NumeroInputs=document.getElementsByTagName('input');
+
+    let acumulador=0;
+    for(let x=0; x<NumeroInputs.length ;x++){
+        acumulador+= Number(NumeroInputs[x].value);
+
+        for(let y=x+1; y<NumeroInputs.length;y++){
+            if(NumeroInputs[x].value==NumeroInputs[y].value){
+                NumeroInputs[y].value=""
+            }
+        }
+
+        MostrarDatos(NumeroInputs);
+
     }
 
+    
+}
+// Para quitar boton de iniciar aplicacion y desplegar inputs
+function EliminarElementos(){
+    while(output.firstChild){
+        output.firstChild.remove();
+    }
+}
+
+function MostrarDatos(NumeroInputsArreglo){
+let strPrint = ''
+    for(let x=0; x<NumeroInputsArreglo.length; x++){
+        if(NumeroInputsArreglo[x].value != ""){
+            strPrint +=`${NumeroInputsArreglo[x].value}   `
+        }
+    }
+Print.innerText = strPrint 
 }
