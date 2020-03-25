605c24073bc3a2e7ad92ee2b36015c55d8a3b4b9
menylizalde <ovnitech2016@gmail.com>
2020-03-24 20:41:06 -0600

    palabras invertidas

--- Arrays/Ej5-Palabra Invertida/app.js
+++ Arrays/Ej5-Palabra Invertida/app.js
@@ -1,93 +1,28 @@
 EventListener();
 
-let arregloNumerosPrimosGlobal = [];
-
-function FillArrayGlobal() {
-    for (let i = 0; i < 1000; i++) {
-        let bandera = true;
-        for (let j = 2; j < i; j++) {
-            if (i % j == 0) {
-                bandera = false;
-                break;
-            }
-        }
-
-        if (bandera) {
-            arregloNumerosPrimosGlobal.push(i);
-        }
-    }
-
-    console.log(arregloNumerosPrimosGlobal);
-}
-
 function EventListener() {
-    document.addEventListener('DOMContentLoaded', FillArrayGlobal);
-    document.getElementById('tamanoArray').addEventListener('keypress', InitApp);
+    document.getElementById('arrayP').addEventListener('keypress', InitApp);
 }
 
-function InitApp(event) {
+function InitApp() {
     if (event.key !== 'Enter') return;
     if (event.target.value == '') return;
-    if (isNaN(event.target.value)) return;
-    if (event.target.value <= 0) return;
 
-    FillArray(event.target.value);
+    arrayInvertido(event.target.value);
 }
 
-function FillArray(tamanoArray) {
+function arrayInvertido(stringArray) {
+    let palabra = [];
+    palabra = stringArray.split('');
+    let palabraInvertida = palabra.reverse().join('').toString();
 
-    let arregloNumerosPrimos = [];
-    let suma = 0;
-    let mayor = 0;
+    console.log(palabraInvertida);
 
-    for (let i = 0; i < tamanoArray; i++) {
-        let numeroAleatorio = Math.floor(Math.random() * (arregloNumerosPrimosGlobal.length - 1)) + 1;
-        arregloNumerosPrimos.push(arregloNumerosPrimosGlobal[numeroAleatorio]);
-        if (arregloNumerosPrimos[i] > mayor) mayor = arregloNumerosPrimos[i];
-    }
+    const output = document.getElementById('output');
+    const div = document.createElement('div');
+    div.setAttribute('class', 'alert alert-success mt-3');
+    div.innerText = palabraInvertida;
 
-    console.log(arregloNumerosPrimos);
+    output.appendChild(div);
 
-    MostrarValores(arregloNumerosPrimos);
-}
-
-function MostrarValores(arregloPrimos) {
-    const listGroup = document.getElementById('listaArreglo');
-    for (let i = 0; i < arregloPrimos.length; i++) {
-        const li = document.createElement('li');
-        li.setAttribute('class', 'list-group-item');
-        li.innerText = `Index ${i + 1} - Valor ${arregloPrimos[i]}`;
-        listGroup.appendChild(li);
-    }
-    let suma = SumaArreglo(arregloPrimos);
-    let mayor = NumeroMayor(arregloPrimos);
-
-    const sumaArreglo = document.createElement('li');
-    sumaArreglo.setAttribute('class', 'list-group-item');
-    sumaArreglo.innerText = `La suma del Arreglo es ${suma}`;
-    listGroup.appendChild(sumaArreglo);
-
-    const numMayor = document.createElement('li');
-    numMayor.setAttribute('class', 'list-group-item');
-    numMayor.innerText = `El numero mayor es ${mayor}`;
-    listGroup.appendChild(numMayor);
-}
-
-function NumeroMayor(arreglo) {
-    let mayor = 0;
-
-    for (let i = 0; i < arreglo.length; i++) {
-        if (arreglo[i] > mayor) mayor = arreglo[i];
-    }
-
-    return mayor;
-}
-
-function SumaArreglo(arreglo) {
-    let suma = 0;
-    for (let i = 0; i < arreglo.length; i++) {
-        suma += arreglo[i];
-    }
-
-    return suma;
 }
